769e8bbb3e0d718fab6bec6df6215d068c856892
Valentin <valydumitru01@yahoo.com>
2022-08-21 00:25:02 +0200

    a

--- javascript/typing_effect.js
+++ javascript/typing_effect.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     // array with texts to type in typewriter
-    var dataText = "Hi.\n My name is\n Valentin\n This is my portfolio \n Enjoy :)";
+    var dataText = "Hi.\n My name is Valentin\n This is my portfolio \n Enjoy :)";
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i) {
