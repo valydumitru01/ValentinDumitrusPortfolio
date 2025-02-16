58897eda4c1bd4748577a46f0cbe4850f52723e6
Valentin <valydumitru01@yahoo.com>
2025-02-16 23:51:40 +0100

    Hotfixes

--- src/global/js/template_handler.js
+++ src/global/js/template_handler.js
@@ -12,12 +12,12 @@ function fillTemplate(template, data) {
 function loadScript(url, className) {
 	return new Promise((resolve, reject) => {
 		const script = document.createElement("script");
-		script.type = "text/javascript";
-		script.src = url;
+		script.type  = "text/javascript";
+		script.src   = url;
 		if (className) {
 			script.classList.add(className);
 		}
-		script.onload = () => resolve(script);
+		script.onload  = () => resolve(script);
 		script.onerror = () => reject(new Error("Failed to load script: " + url));
 		document.head.appendChild(script);
 	});
@@ -25,17 +25,20 @@ function loadScript(url, className) {
 
 function generate_page(page) {
 	document.querySelectorAll(".dynamic-js").forEach(el => el.remove());
-	const oldCss = document.querySelector("#dynamic-css");
-	if (oldCss) oldCss.remove();
 	
-	const cssLink = document.createElement("link");
-	cssLink.rel = "stylesheet";
-	cssLink.id = "dynamic-css";
-	cssLink.href = basePath + "src/pages/" + history.state + "/css/style.css";
-	document.head.appendChild(cssLink);
 	
 	loadScript(basePath + "src/pages/" + history.state + "/json/data.js", "dynamic-js")
-		.then(() => loadScript(basePath + "src/pages/" + history.state + "/html/html.js", "dynamic-js"))
+		.then(() => {
+			loadScript(basePath + "src/pages/" + history.state + "/html/html.js", "dynamic-js")
+			const oldCss = document.querySelector("#dynamic-css");
+			if (oldCss) oldCss.remove();
+			
+			const cssLink = document.createElement("link");
+			cssLink.rel   = "stylesheet";
+			cssLink.id    = "dynamic-css";
+			cssLink.href  = basePath + "src/pages/" + history.state + "/css/style.css";
+			document.head.appendChild(cssLink);
+		})
 		.then(() => {
 			const generator = window["generate_page_" + page];
 			if (typeof generator === "function") {
