7fd57513b9783defd30decf4d1c08d2aea6aa973
Valentin <valydumitru01@yahoo.com>
2025-08-19 20:11:41 +0200

    cv

--- src/pages/myexperience/json/data.js
+++ src/pages/myexperience/json/data.js
@@ -2,12 +2,27 @@ DATA = [
 	{
 		icon       : "fa-briefcase",
 		period     : {
-			start: "Dec. 2024",
+			start: "Jun. 2025",
 			end  : "Present",
 			duration: "Present"
 		},
+		title      : "Senior Rust Software Engineer",
+		description: "I am currently working as a Senior Rust Software Engineer at GMV, developing a rule engine from scratch."
+					 + "It includes parsing, compiling and designing rules, to then execute over a set of data."
+					 + "I am also in charge of designing the architecture of the system, and implementing the core features."
+					 + "I am also responsible for the performance and scalability of the system, ensuring the rules can be executed efficiently over large amounts of data."
+					 + "I am also dealing with a lot of informal requirements and communication, to then translate into formal code and documentation."
+					 + "Finally, I am also mentoring junior developers and helping them grow in their careers."
+	},
+	{
+		icon       : "fa-briefcase",
+		period     : {
+			start: "Dec. 2024",
+			end  : "Jun. 2025",
+			duration: "7 months"
+		},
 		title      : "C++ Software Engineer",
-		description: "I am currently working as a C++ developer and engineer in a critical mission telecommunications company. Responsibilities include documenting, architecting, and developing a full-stack website using Angular and C++ and the software of critical mission soft switches developed in C++. I overhauled one of their systems, drastically improving performance, maintainability, and efficiency."
+		description: "I worked as a C++ developer and engineer in a critical mission telecommunications company. Responsibilities included documenting, architecting, and developing a full-stack website using Angular and C++ and the software of critical mission soft switches developed in C++. I overhauled one of their systems, drastically improving performance, maintainability, and efficiency."
 	}, {
 		icon       : "fa-briefcase",
 		period     : {
@@ -18,7 +33,24 @@ DATA = [
 		title      : "Game engine development (Personal Project)",
 		description: "I developed a game engine from scratch using C++ and OpenGL. It includes a graphic API abstraction, fully custom math library, a simple rendering, sound, input and physics system and some other features. GitHub repo <a href='https://github.com/valydumitru01/GLESC'> here</a>."
 		
-	}, {
+	},
+	{
+		icon       : "fa-briefcase",
+		period     : {
+			start   : "Feb. 2023",
+			end     : "Apr. 2023",
+			duration: "3 months"
+		},
+		title      : "Electronic Arts - Part Time Contract",
+		subtitle   : "Game Engine Developer",
+		description: "- Assisted in developing and maintaining C++ engine modules for rendering and physics systems.\n"
+					 + "- Optimized engine subsystems to improve frame rate consistency and memory usage.\n"
+					 + "- Supported integration of new debugging and profiling tools for developers.\n"
+					 + "- Collaborated with gameplay and tools teams to resolve engine-related issues.\n"
+					 + "- Participated in code reviews and contributed to internal documentation."
+		
+	},
+	{
 		icon       : "fa-briefcase",
 		period     : {
 			start   : "Feb. 2023",
